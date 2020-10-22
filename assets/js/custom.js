1fc593397363cec85d9100c14de2913bcdeea622
afenker1985 <aaronfenker@gmail.com>
2020-10-22 08:40:06 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -48,7 +48,7 @@
 		var typed = new Typed('#typed', {
 		    stringsElement: '#typed-strings',
 		    typeSpeed: 20,
-			backSpeed: 30,
+			backSpeed: 50,
 			backDelay: 500,
 		    startDelay: 1000,
 		    loop: true,
